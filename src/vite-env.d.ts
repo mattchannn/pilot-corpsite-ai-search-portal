b83f83b7f1cfa7ecab93a64cdee39532a4ab24c2
Mat <klchan.358@gmail.com>
2026-03-23 18:00:37 +0800

    chore: try triggering deployment

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,6 +1,7 @@
 /// <reference types="vite/client" />
 
 // https://vite.dev/guide/env-and-mode#intellisense-for-typescript
+// .
 interface ImportMetaEnv {
 	readonly VITE_AI_SEARCH_API_URL: string
 }
