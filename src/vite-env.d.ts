9e7d96f964875506d13bdd3f5be16d015bb80a62
Mat <klchan.358@gmail.com>
2026-03-23 18:01:47 +0800

    rollback

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,7 +1,6 @@
 /// <reference types="vite/client" />
 
 // https://vite.dev/guide/env-and-mode#intellisense-for-typescript
-// .
 interface ImportMetaEnv {
 	readonly VITE_AI_SEARCH_API_URL: string
 }
